17baa686493f12e324015688aefcaf17ad49efbe
LucidT <laaxid@gmail.com>
2022-05-19 19:11:13 -0600

    About to redo the physics
    This doesn't have delta

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,92 +8,77 @@ export var MAXFALLSPEED = 900
 export var ACCEL = 20
 export var DEACCEL = 50
 var jump_force = -700
-var force = Vector2()
+var velocity = Vector2()
 var double_jump = 1
 var facing = 0
 
 func _physics_process(delta):
 	
-	motion_physics()
+	motion_physics(delta)
 	get_walking_sounds()
 	jumping_physics()
 	
-	force.y += GRAVITY
-	if force.y > MAXFALLSPEED:
-		force.y = MAXFALLSPEED
-	if Input.is_action_just_released("jump"):
-		force.y += GRAVITY*7
-	# end physics
-	print (force)
+	print (velocity)
 
 func get_walking_sounds():
-	if force.x != 0 and is_on_floor():
+	if velocity.x != 0 and is_on_floor():
 		if !$WalkingSound.is_playing():
 			$WalkingSound.play()
 	else:
 		$WalkingSound.stop()
 
 #this contains left to right inputs, acceleration, and deacceleration
-func motion_physics():
-	force.x = clamp(force.x,-BASESPEED,BASESPEED)
+func motion_physics(delta):
 	if Input.is_action_pressed("move_right"):
-		force.x += ACCEL
+		velocity.x += ACCEL
 		$AnimatedSprite.flip_h = false
 	elif Input.is_action_pressed("move_left"):
-			force.x -= ACCEL
+			velocity.x -= ACCEL
 			$AnimatedSprite.flip_h = true
 	#This is the motion deacceleration
 	else:
-		if abs(force.x) < 40:
-			force.x= 0 
+		if abs(velocity.x) < 40:
+			velocity.x= 0 
 		#Air Deacceleration
 		elif ! is_on_floor():
-			if force.x > 0:
-				force.x -= ACCEL
-			elif force.x < 0:
-				force.x += ACCEL
+			if velocity.x > 0:
+				velocity.x -= ACCEL
+			elif velocity.x < 0:
+				velocity.x += ACCEL
 		#Ground Deacceleration
 		elif is_on_floor():
-			if force.x > 0:
-				force.x -= DEACCEL
-			elif force.x < 0:
-				force.x += DEACCEL
-		else:
-			force.x= 0
+			if velocity.x > 0:
+				velocity.x -= DEACCEL
+			elif velocity.x < 0:
+				velocity.x += DEACCEL
+	velocity.x = clamp(velocity.x,-BASESPEED,BASESPEED)
 	
 	#this fixes a bug where pivoting causes you to slide more when having both buttons pressed.
 	if Input.is_action_pressed("move_right") and Input.is_action_pressed("move_left") and is_on_floor():
-		if force.x > 0:
-			force.x -= DEACCEL
-		elif force.x < 0:
-			force.x += DEACCEL
-	move_and_slide(force,Vector2(0,-1))
+		if velocity.x > 0:
+			velocity.x -= DEACCEL
+		elif velocity.x < 0:
+			velocity.x += DEACCEL
+	move_and_slide(velocity,Vector2(0,-1))
 
 # This also contains the wall sliding physics which are currently broken
 ## To-do's: try and reimplement wall friction without breaking the jump
 func jumping_physics():
-	if is_on_floor():
-		force.y = 200
-		if Input.is_action_just_pressed("jump"):
-			force.y = BASEJUMP
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = BASEJUMP
 			$JumpingSound.play()
 			double_jump = 1
 			if Input.is_action_pressed("jump"):
-				force.y = jump_force
-	if is_on_wall():
-		
-		# need to fix. Makes you jump too high:
-		if !is_on_floor():
-			force.y -= 10
-		if Input.is_action_just_pressed("jump") and double_jump == 1 and ! is_on_floor():
+				velocity.y = jump_force
+		if Input.is_action_just_pressed("jump") and is_on_wall() and double_jump == 1 and ! is_on_floor():
 			get_facing_direction()
-			force.x = 300*facing
-			force.y = jump_force * .90
+			velocity.x = 300*facing
+			velocity.y = jump_force * .90
 			$JumpingSound.play()
 			double_jump = 0
 
 func get_facing_direction():
-	if force.x > 0:
+	if velocity.x > 0:
 		facing = -1
-	if force.x < 0:
+	if velocity.x < 0:
 		facing = 1
